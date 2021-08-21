e5f4a372e767ec6a10e26ecc175373ec193ab277
cyyspring <38151303+cyyspring@users.noreply.github.com>
2021-08-21 11:54:55 +0800

    Add files via upload

---
+++ src/example/baseExample.js
@@ -0,0 +1,54 @@
+import ObjectBase from '../core/FormBase'
+import SerializeUtils from '../core/SerializeUtils'
+
+// 指定字段映射对应类
+
+class Info {
+    infoName = 'infoName'
+}
+
+class Person extends ObjectBase {
+    name = 'json'
+    @SerializeUtils.jsonType(Info)
+    info = []
+}
+
+class A extends ObjectBase {
+    name = '123'
+    age = '456'
+    ls = [1, 2, 3]
+    ls1 = [{ name: 13 }]
+    obj = new Person()
+
+    @SerializeUtils.jsonType(Person)
+    personLs = []
+    obj1 = { ls: [1, 3] }
+    _aaa = 'zzz'
+
+    testDeepClone() {
+        const { length } = this.personLs
+        if (length) {
+            this.personLs[0].name = 'clone'
+        }
+    }
+}
+
+// ----------测试数据------------
+const api = {
+    name: 'api',
+    age: 'api',
+    ls: [1],
+    ls1: [{ name: 'api' }],
+    obj: { name: 'api', age: 'api', info: [{ infoName: 'apiobj', name: 'api', age: 'api' }] },
+    personLs: [
+        { name: 'api', age: 'api', info: [{ infoName: 'api', name: 'api', age: 'api' }] },
+        { name: 'api', age: 'api' },
+    ],
+}
+
+const a = new A().init(api)
+a.testDeepClone()
+
+console.log('api', JSON.stringify(api, null, 4))
+
+console.log('a', JSON.stringify(a, null, 4))
---
+++ src/example/jsonField.js
@@ -0,0 +1,50 @@
+import ObjectBase from '../core/FormBase'
+import SerializeUtils from '../core/SerializeUtils'
+
+// 指定字段映射对应类
+
+class Info {
+    @SerializeUtils.jsonField('newInfoName')
+    infoName = 'infoName'
+}
+
+class Person extends ObjectBase {
+    name = 'json'
+
+    @SerializeUtils.jsonType(Info)
+    info = []
+}
+
+class A extends ObjectBase {
+    @SerializeUtils.jsonField('newName')
+    name = '123'
+
+    @SerializeUtils.jsonType(Person)
+    personLs = []
+
+    // 输出时候key 不变
+    @SerializeUtils.jsonField('newFORMATTER', false)
+    FORMATTER = '2'
+    _zz = '转换后将不输出'
+}
+
+// ----------测试数据------------
+const api = {
+    newName: 'api',
+    personLs: [{ name: 'api', info: [{ newInfoName: 'api', age: 15 }] }],
+    newEXTEND: 'api',
+    newFORMATTER: 'api',
+}
+
+const a = new A().init(api)
+
+console.log('api', JSON.stringify(api, null, 4))
+
+console.log('a', JSON.stringify(a, null, 4))
+
+console.log('a', JSON.stringify(a.formateObj(), null, 4))
+
+console.log('a', JSON.stringify(new A().shallowInit(api), null, 4))
+
+
+
